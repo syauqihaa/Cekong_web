dc1dc50b1ada1b488c9be18274474d5a7c1687a9
syauqihaa <syauqiha98@gmail.com>
2022-06-23 21:29:41 +0700

    laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js([
+    'resources/js/app.js',
+    'resources/js/ongkir.js'
+], 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
